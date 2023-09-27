0cfd2d089345a1ab62201ad226c5c73b495bb830
Jorge Fonseca <jorge.fonseca87@gmail.com>
2023-09-27 17:03:51 -0400

    first commit

---
+++ WebApplication5/Controllers/TestController.cs
@@ -0,0 +1,61 @@
+﻿using DinkToPdf;
+using DinkToPdf.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IConverter _converter;
+        private const string imgUrl = @"C:\Users\jorfonse\Pictures\IMG_20220125_115833.jpg";
+
+        public TestController(IConverter converter)
+        {
+            _converter = converter;
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                var globalSettings = new GlobalSettings
+                {
+                    ColorMode = ColorMode.Color,
+                    Orientation = Orientation.Portrait,
+                    PaperSize = PaperKind.A4,
+                    Margins = new MarginSettings { Top = 10 },
+                    DocumentTitle = "PDF Report",
+                    Out = @"C:\PDFCreator\Employee_Report.pdf"
+                };
+                var objectSettings = new ObjectSettings
+                {
+                    PagesCount = true,
+                    HtmlContent = @$"<img src='{imgUrl}' style='width:100%; -webkit-transform: none;'/>",
+                    WebSettings = { DefaultEncoding = "utf-8" },
+                    HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                    FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
+                };
+                var pdf = new HtmlToPdfDocument()
+                {
+                    GlobalSettings = globalSettings,
+                    Objects = { objectSettings }
+                };
+                _converter.Convert(pdf);
+                return Ok("Successfully created PDF document.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+        }
+    }
+}
